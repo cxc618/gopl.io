0c6ac3fbcde94b7bb46fcd3fd3db1a997d9e00c7
chenchao <qqwwee>
2021-04-19 10:15:59 +0800

    handle error

--- my/basic/greetings/greetings.go
+++ my/basic/greetings/greetings.go
@@ -1,8 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v", name)
-	return message
+func Hello(name string) (string, error) {
+	message := fmt.Sprintf("Hi, %v\n", name)
+
+	if name == "error" {
+		return "", errors.New("empty name")
+	}
+	return message, nil
 }
--- my/basic/hello/hello.go
+++ my/basic/hello/hello.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"gopl.io/my/basic/greetings"
+	"log"
 	"os"
 )
 
 func main()  {
+	message, err := greetings.Hello(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(message)
+
 	if len(os.Args) == 2 {
-		message := greetings.Hello(os.Args[1])
-		fmt.Printf(message)
 	} else {
 		fmt.Printf("error args %s\n", os.Args)
 	}
