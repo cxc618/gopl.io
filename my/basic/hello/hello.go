74b36e34f8d55866ef749e7f90b39d18ef42eeb3
chenchao <qqwwee>
2021-04-19 10:32:52 +0800

    use map

--- my/basic/hello/hello.go
+++ my/basic/hello/hello.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main()  {
-	message, err := greetings.Hello(os.Args[1])
+	//message, err := greetings.Hello(os.Args[1])
+	names := []string{"cxc", "hh", "zz"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf(message)
+	fmt.Println(messages)
 
 	if len(os.Args) == 2 {
 	} else {
