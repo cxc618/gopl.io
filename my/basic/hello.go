b6e6e05512be64a703f2ecf59af53f99571087f9
chenchao <qqwwee>
2021-04-19 09:09:22 +0800

    add gitignore

--- my/basic/hello.go
+++ my/basic/hello.go
@@ -6,8 +6,7 @@ import (
 )
 
 func main()  {
-	len := len(os.Args)
-	if (len == 2) {
+	if len(os.Args) == 2 {
 		fmt.Printf("Hello, %s\n", os.Args[1])
 	} else {
 		fmt.Printf("error args %s\n", os.Args)
